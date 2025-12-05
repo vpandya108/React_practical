4513c5dc585924b1513de140f6b72f15f950ecd0
vpandya108 <vpandya108@rku.ac.in>
2025-12-05 19:11:04 +0530

    add myvariable to App component for dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import './styles.css'
 
 function App() {
+  let myvariable="vishva";
   return (
     <div>
+      <div>{myvariable}</div>
       <h1>Hello, react!</h1>
       <h5>learning react</h5>
       <button>click me!</button>
