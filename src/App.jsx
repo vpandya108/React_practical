74d31f9f541576f664373f1d6c45d11d3923d064
vpandya108 <vpandya108@rku.ac.in>
2025-12-12 20:23:57 +0530

    refactor App component for improved structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import { useState } from 'react'
 import './styles.css'
+import Login,{Profile,Setting, userkey}  from './second';
 
 function App() {
-  alert(sum(5,6));
+  //alert(sum(5,6));
+  const name ="vishva pandya";
+  let x=50;
+  let y=40;
   let myvariable="vishva";
+  function hello()
+  {
+    return "greeting"
+  }
   return (
     <div>
       <div>{myvariable}</div>
-      <h1>Hello, react!</h1>
+      <h1>Hello, react!{sum(10,30)}</h1>
+      <h1>{name}</h1>
+      <h1>{x*y}</h1>
+      {hello()}
       <h5>learning react</h5>
-      <button>click me!</button>
+      <button onClick={()=>alert("helloo")}>click me!</button>
+      <br></br>
+      <h1>{userkey}</h1>
       <p>my paragraph</p>
       <Color />
+
       <Paint />
+      <Login />
+      <Profile />
+      <Setting/>
     </div>
   )
 }
+//component function
 function Color()
 {
   return(
@@ -30,8 +48,10 @@ function Paint()
     </div>
   )
 }
+//normal function
 function sum(a,b)
 {
   return a+b
 }
+
 export default App
